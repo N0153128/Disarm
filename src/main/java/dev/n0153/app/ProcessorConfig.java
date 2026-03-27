b5a10f0aad4104f17424edd3deb426d0b13535b0
N0153128 <noisebro@hotmail.com>
2026-03-27 21:43:31 +0000

    ProcessorConfig: added methods and docs.

--- src/main/java/dev/n0153/app/ProcessorConfig.java
+++ src/main/java/dev/n0153/app/ProcessorConfig.java
@@ -1,7 +1,45 @@
 package dev.n0153.app;
 
+import java.util.Set;
+
 /**
  * This interface holds configuration for processors
  */
 public interface ProcessorConfig {
+
+    /**
+     * Every plugin must contain its own unique ID.
+     * @return unique ID
+     */
+    int getId();
+
+    /**
+     * Every plugin must contain its own version number.
+     * @return version number
+     */
+    double getVersion();
+
+    /**
+     * Describes supported formats and mime types
+     */
+    enum Supports{};
+
+    /**
+     * Describes constrains of the supported media types and formats
+     */
+    enum MediaConstrains{};
+
+    /**
+     * Provides maximum allowed file size if global size limit wasn't applied.
+     * @return maximum allowed file size for specified mime
+     */
+    int maxFileSizeInBytes(String mime);
+
+    /**
+     * Specifies log level for the plugin: Info, Debug, Warning, Error.
+     * @return Log level
+     */
+    String getLogLevel();
+
+
 }
