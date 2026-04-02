561fef04bfe9da50c2e62781d2aae946e78168d7
N0153128 <noisebro@hotmail.com>
2026-04-02 04:39:16 +0100

    implemented ImageConfig

--- src/main/java/dev/n0153/app/plugins/image/ImageConfig.java
+++ src/main/java/dev/n0153/app/plugins/image/ImageConfig.java
@@ -4,36 +4,115 @@ import dev.n0153.app.ProcessorConfig;
 
 public class ImageConfig implements ProcessorConfig {
 
-    private final String name = "Image";
-    private final int id = 1;
-    private final int version = 1;
-    private final int maxFileSizeInBytes = 5_000_000;
-    private final String logLevel = "info";
-
     @Override
     public String getName() {
-        return name;
+        return "Image";
     }
 
     @Override
     public int getId() {
-        return id;
+        return 1;
     }
 
     @Override
     public double getVersion() {
-        return version;
+        return 1;
     }
 
     @Override
     public int maxFileSizeInBytes(String mime) {
-        return maxFileSizeInBytes;
+        return 5_000_000;
     }
 
     @Override
     public String getLogLevel() {
-        return logLevel;
+        return "info";
     }
 
+    private final int logoSizeLimit; //5MB
+    private final boolean keepLogo;
+    private final boolean keepImage;
+    private final double imgMaxWidth;
+    private final double imgMaxHeight;
+    private final int logoMaxWidth;
+    private final int logoMaxHeight;
 
+    private ImageConfig(Builder builder) {
+        this.logoSizeLimit = builder.logoSizeLimit;
+        this.keepLogo = builder.keepLogo;
+        this.keepImage = builder.keepImage;
+        this.imgMaxWidth = builder.imgMaxWidth;
+        this.imgMaxHeight = builder.imgMaxHeight;
+        this.logoMaxWidth = builder.logoMaxWidth;
+        this.logoMaxHeight = builder.logoMaxHeight;
+    }
+
+    //getters
+    public int getLogoSizeLimit() {
+        return logoSizeLimit;
+    }
+
+    public boolean isKeepLogo() {
+        return keepLogo;
+    }
+
+    public boolean isKeepImage() {
+        return keepImage;
+    }
+
+    public double getImgMaxWidth() {
+        return imgMaxWidth;
+    }
+
+    public double getImgMaxHeight() {
+        return imgMaxHeight;
+    }
+
+    public int getLogoMaxWidth() {
+        return logoMaxWidth;
+    }
+
+    public int getLogoMaxHeight() {
+        return logoMaxHeight;
+    }
+
+    public static class Builder {
+        private int logoSizeLimit; //5MB
+        private boolean keepLogo;
+        private boolean keepImage;
+        private double imgMaxWidth;
+        private double imgMaxHeight;
+        private int logoMaxWidth;
+        private int logoMaxHeight;
+
+        //setters
+        public void setLogoSizeLimit(int logoSizeLimit) {
+            this.logoSizeLimit = logoSizeLimit;
+        }
+
+        public void setKeepLogo(boolean keepLogo) {
+            this.keepLogo = keepLogo;
+        }
+
+        public void setKeepImage(boolean keepImage) {
+            this.keepImage = keepImage;
+        }
+
+        public void setImgMaxWidth(double imgMaxWidth) {
+            this.imgMaxWidth = imgMaxWidth;
+        }
+
+        public void setImgMaxHeight(double imgMaxHeight) {
+            this.imgMaxHeight = imgMaxHeight;
+        }
+
+        public void setLogoMaxWidth(int logoMaxWidth) {
+            this.logoMaxWidth = logoMaxWidth;
+        }
+
+        public void setLogoMaxHeight(int logoMaxHeight) {
+            this.logoMaxHeight = logoMaxHeight;
+        }
+
+    }
 }
