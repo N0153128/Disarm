ce24e6c8281dc720812a92e2a46062836c5282ab
N0153128 <noisebro@hotmail.com>
2026-04-05 23:59:38 +0100

    added basic config fields to GlobalConfig placeholder

--- src/main/java/dev/n0153/app/GlobalConfig.java
+++ src/main/java/dev/n0153/app/GlobalConfig.java
@@ -1,5 +1,12 @@
 package dev.n0153.app;
 
+import java.nio.file.Path;
+
 public class GlobalConfig {
+    private final int generalSizeLimit;
+    private final boolean keepInputs;
+    private final boolean keepOriginal;
+    private final int generalFileSizeUpperBoundLimit;
+    private final Path generalOutputPath;
 
 }
