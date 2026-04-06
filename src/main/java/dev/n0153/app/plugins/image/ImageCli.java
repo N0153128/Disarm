516dbf5275c4a885f744bf8c74aa39e416cfc475
N0153128 <noisebro@hotmail.com>
2026-04-06 23:12:28 +0100

    implemented ImageCli

---
+++ src/main/java/dev/n0153/app/plugins/image/ImageCli.java
@@ -0,0 +1,34 @@
+package dev.n0153.app.plugins.image;
+
+import dev.n0153.app.CliConfig;
+import dev.n0153.app.ProcessorConfig;
+
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class ImageCli implements CliConfig<ImageConfig, ImageConfig.Builder> {
+    private static final Map<String, BiConsumer<ImageConfig.Builder, String>> PARAM_STORAGE = Map.of(
+            "logo-size-limit", ((builder, value) -> builder.setLogoSizeLimit(Integer.parseInt(value))),
+            "keep-logo", ((builder, value) -> builder.setKeepLogo(Boolean.parseBoolean(value))),
+            "keep-image", ((builder, value) -> builder.setKeepImage(Boolean.parseBoolean(value))),
+            "image-max-width", ((builder, value) -> builder.setImgMaxWidth(Integer.parseInt(value))),
+            "image-max-height", ((builder, value) -> builder.setImgMaxHeight(Integer.parseInt(value))),
+            "logo-max-width", ((builder, value) -> builder.setLogoMaxWidth(Integer.parseInt(value))),
+            "logo-max-height", ((builder, value) -> builder.setLogoMaxHeight(Integer.parseInt(value)))
+            );
+
+    @Override
+    public Map<String, BiConsumer<ImageConfig.Builder, String>> paramStorage() {
+        return PARAM_STORAGE;
+    }
+
+    @Override
+    public ImageConfig.Builder createBuilder() {
+        return new ImageConfig.Builder();
+    }
+
+    @Override
+    public ImageConfig build(ImageConfig.Builder builder) {
+        return builder.build();
+    }
+}
