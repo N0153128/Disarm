a9db419c74de2ae4faeb87aa080fe695136b6561
N0153128 <noisebro@hotmail.com>
2026-05-30 18:50:26 +0100

    TextCLI: implemented TextCLI

--- src/main/java/dev/n0153/app/plugins/text/TextCLI.java
+++ src/main/java/dev/n0153/app/plugins/text/TextCLI.java
@@ -1,4 +1,37 @@
 package dev.n0153.app.plugins.text;
 
-public class TextCLI {
+import dev.n0153.app.DisarmCLI;
+import dev.n0153.app.PluginRegistry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+
+@Command(name = "text", description = "Text processing plugin ")
+public class TextCLI  implements Runnable {
+    private static final Logger logger = LogManager.getLogger(TextCLI.class);
+
+    private final TextConfig config = new TextConfig();
+    private final PluginRegistry registry;
+
+    public TextCLI(PluginRegistry registry) {
+        this.registry = registry;
+        registry.updateConfig("text", config);
+    }
+
+    @CommandLine.ParentCommand
+    DisarmCLI inputPath;
+
+    @CommandLine.Option(names = {"-mts", "--max-text-size"})
+    private int textSize;
+
+    @Override
+    public void run() {
+        if (textSize > 0) {
+            this.config.setMaxTextSize(textSize);
+        }
+
+        registry.updateConfig("text", config);
+    }
 }
