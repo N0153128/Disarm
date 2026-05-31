9f93e630bd73b47479af95f53d0df71e83dc9dce
N0153128 <noisebro@hotmail.com>
2026-05-31 16:04:45 +0100

    DisarmPlugins: registered Text plugin

--- src/main/java/dev/n0153/app/plugins/DisarmPlugins.java
+++ src/main/java/dev/n0153/app/plugins/DisarmPlugins.java
@@ -6,11 +6,15 @@ import dev.n0153.app.MediaPlugin;
 import dev.n0153.app.PluginRegistry;
 import dev.n0153.app.plugins.image.ImageConfig;
 import dev.n0153.app.plugins.image.ImagePlugin;
+import dev.n0153.app.plugins.text.TextConfig;
+import dev.n0153.app.plugins.text.TextPlugin;
 
 public class DisarmPlugins {
     public void registerAll(PluginRegistry registry) {
         ImageConfig imageConfig = new ImageConfig();
         new ImagePlugin().register(registry, imageConfig);
+        TextConfig textConfig = new TextConfig();
+        new TextPlugin().register(registry, textConfig);
         //add new plugins as they become available
     }
 }
