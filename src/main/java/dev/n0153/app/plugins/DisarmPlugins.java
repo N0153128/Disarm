8d89a8b54faec8640dae03e4f5dd9985867e2454
N0153128 <noisebro@hotmail.com>
2026-06-09 19:19:46 +0100

    DisarmPlugins: registered Audio plugin

--- src/main/java/dev/n0153/app/plugins/DisarmPlugins.java
+++ src/main/java/dev/n0153/app/plugins/DisarmPlugins.java
@@ -4,6 +4,8 @@ import dev.n0153.app.GlobalConfig;
 import dev.n0153.app.MediaConfig;
 import dev.n0153.app.MediaPlugin;
 import dev.n0153.app.PluginRegistry;
+import dev.n0153.app.plugins.audio.AudioConfig;
+import dev.n0153.app.plugins.audio.AudioPlugin;
 import dev.n0153.app.plugins.image.ImageConfig;
 import dev.n0153.app.plugins.image.ImagePlugin;
 import dev.n0153.app.plugins.text.TextConfig;
@@ -25,6 +27,11 @@ public class DisarmPlugins {
         TextPlugin text = new TextPlugin();
         text.registerGlobalConfig(globalConfig);
         text.register(registry, textConfig);
+
+        AudioConfig audioConfig = new AudioConfig();
+        AudioPlugin audioPlugin = new AudioPlugin();
+        audioPlugin.registerGlobalConfig(globalConfig);
+        audioPlugin.register(registry, audioConfig);
         //add new plugins as they become available
     }
 }
