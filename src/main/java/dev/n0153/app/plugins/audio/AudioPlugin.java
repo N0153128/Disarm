b86829f4e8e869efa01dfcb83b9a05f4904ddba9
N0153128 <noisebro@hotmail.com>
2026-06-09 02:47:51 +0100

    AudioPlugin: implemented AudioPlugin class

--- src/main/java/dev/n0153/app/plugins/audio/AudioPlugin.java
+++ src/main/java/dev/n0153/app/plugins/audio/AudioPlugin.java
@@ -1,4 +1,57 @@
 package dev.n0153.app.plugins.audio;
 
-public class AudioPlugin {
+import dev.n0153.app.*;
+import dev.n0153.app.exceptions.DisarmException;
+
+public class AudioPlugin implements MediaPlugin {
+    private MediaConfig config;
+    private final AudioContext context = new AudioContext();
+    private final AudioValidator validator = new AudioValidator();
+    private GlobalConfig globalConfig;
+
+    @Override
+    public GlobalConfig getGlobalConfig() {
+        return globalConfig;
+    }
+
+    @Override
+    public MediaProcessor<?> getProcessor(MediaConfig config) {
+        ensureGlobalConfig();
+        return new AudioProcessor((AudioConfig) config, context, globalConfig);
+    }
+
+    @Override
+    public MediaValidator getValidator() {
+        validator.createMeta((AudioConfig) config, context);
+        return validator;
+    }
+
+    @Override
+    public MediaConfig getConfig() {
+        return config;
+    }
+
+    @Override
+    public void register(PluginRegistry registry, MediaConfig config) throws DisarmException {
+        if (ensureGlobalConfig()) {
+            this.config = config;
+            registry.register(
+                    config.supports(),
+                    this,
+                    new AudioCLI(registry, context),
+                    "audio",
+                    config
+            );
+        }
+    }
+
+    @Override
+    public void registerGlobalConfig(GlobalConfig globalConfig) {
+        this.globalConfig = globalConfig;
+    }
+
+    @Override
+    public String echo() {
+        return "audio";
+    }
 }
