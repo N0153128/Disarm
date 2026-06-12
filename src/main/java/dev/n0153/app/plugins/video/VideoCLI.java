25d35b112ca3348f22ed115a5883cb17f6c96b91
N0153128 <noisebro@hotmail.com>
2026-06-12 11:00:17 +0100

    VideoCLI: implemented placeholder class

--- src/main/java/dev/n0153/app/plugins/video/VideoCLI.java
+++ src/main/java/dev/n0153/app/plugins/video/VideoCLI.java
@@ -1,4 +1,30 @@
 package dev.n0153.app.plugins.video;
 
-public class VideoCLI {
+import dev.n0153.app.DisarmCLI;
+import dev.n0153.app.PluginRegistry;
+import dev.n0153.app.plugins.audio.AudioCLI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "video", description = "Video Processing Plugin")
+public class VideoCLI implements Runnable {
+    private static final Logger logger = LogManager.getLogger(VideoCLI.class);
+
+    private final VideoConfig config = new VideoConfig();
+    private final PluginRegistry registry;
+
+    public VideoCLI(PluginRegistry registry) {
+        this.registry = registry;
+        registry.updateConfig("video", config);
+    }
+
+    @CommandLine.ParentCommand
+    DisarmCLI inputPath;
+
+
+    @Override
+    public void run() {
+
+    }
 }
