588e44049374943831b136baf1c4cda08c33c577
N0153128 <noisebro@hotmail.com>
2026-06-23 14:30:32 +0100

    VideoPlugin: implemented VideoPlugin class

--- src/main/java/dev/n0153/app/plugins/video/VideoPlugin.java
+++ src/main/java/dev/n0153/app/plugins/video/VideoPlugin.java
@@ -1,4 +1,58 @@
 package dev.n0153.app.plugins.video;
 
-public class VideoPlugin {
+import dev.n0153.app.*;
+import dev.n0153.app.exceptions.DisarmException;
+import dev.n0153.app.plugins.audio.AudioCLI;
+
+public class VideoPlugin implements MediaPlugin {
+    private MediaConfig config;
+    private final VideoContext context = new VideoContext();
+    private final VideoValidator validator = new VideoValidator();
+    private GlobalConfig globalConfig;
+
+    @Override
+    public GlobalConfig getGlobalConfig() {
+        return globalConfig;
+    }
+
+    @Override
+    public MediaProcessor<?> getProcessor(MediaConfig config) {
+        ensureGlobalConfig();
+        return new VideoProcessor((VideoConfig) config, context, globalConfig);
+    }
+
+    @Override
+    public MediaValidator getValidator() {
+        validator.createMeta((VideoConfig) config, context);
+        return validator;
+    }
+
+    @Override
+    public MediaConfig getConfig() {
+        return config;
+    }
+
+    @Override
+    public void register(PluginRegistry registry, MediaConfig config) throws DisarmException {
+        if (ensureGlobalConfig()) {
+            this.config = config;
+            registry.register(
+                    config.supports(),
+                    this,
+                    new AudioCLI(registry),
+                    "video",
+                    config
+            );
+        }
+    }
+
+    @Override
+    public void registerGlobalConfig(GlobalConfig globalConfig) {
+        this.globalConfig = globalConfig;
+    }
+
+    @Override
+    public String echo() {
+        return "video";
+    }
 }
