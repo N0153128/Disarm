0e4eb8471e95242bd75b1fc380bcffb823a7678f
N0153128 <noisebro@hotmail.com>
2026-07-05 00:52:20 +0100

    VideoUtilsTest: implemented testdir, fixed reference calls

--- src/test/java/dev/n0153/app/VideoUtilsTest.java
+++ src/test/java/dev/n0153/app/VideoUtilsTest.java
@@ -1,25 +1,32 @@
 package dev.n0153.app;
 
+import dev.n0153.app.plugins.MediaUtils;
+import dev.n0153.app.plugins.video.VideoUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import ws.schild.jave.EncoderException;
 import ws.schild.jave.info.VideoSize;
 
 import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class VideoUtilsTest {
 
+    @TempDir
+    Path tempDir;
+
     @Test
     void getCodec_forVideo() {
-        String codec = assertDoesNotThrow(() -> Utils.getCodec(DebugPaths.videoTestInputMp4, "video"));
+        String codec = assertDoesNotThrow(() -> MediaUtils.getCodec(DebugPaths.videoTestInputMp4, "video"));
         assertNotNull(codec);
         assertFalse(codec.isEmpty());
     }
 
     @Test
     void getBitrate_forVideo() {
-        int bitrate = assertDoesNotThrow(() -> Utils.getBitrate(
+        int bitrate = assertDoesNotThrow(() -> MediaUtils.getBitrate(
                 DebugPaths.videoTestInputMp4, "video"));
         assertNotEquals(0, bitrate);
     }
@@ -27,55 +34,55 @@ public class VideoUtilsTest {
     @Test
     void getSamplingRate_withVideo() {
         int sampling = assertDoesNotThrow(() ->
-                Utils.getSamplingRate(DebugPaths.videoTestInputMp4));
+                MediaUtils.getSamplingRate(DebugPaths.videoTestInputMp4));
         assertTrue(sampling > 0);
     }
 
     @Test
     void calcVideoBitRate_withWebm() {
         int bitrate = assertDoesNotThrow(() ->
-                Utils.calcVideoBitRate(DebugPaths.videoTestInputWebm));
+                MediaUtils.calcVideoBitRate(DebugPaths.videoTestInputWebm));
         assertTrue(bitrate > 0);
     }
 
     @Test
     void calcVideoBitRate_withMkv() {
         int bitrate = assertDoesNotThrow(() ->
-                Utils.calcVideoBitRate(DebugPaths.videoTestInputMkv));
+                MediaUtils.calcVideoBitRate(DebugPaths.videoTestInputMkv));
         assertTrue(bitrate > 0);
     }
 
     @Test
     void calcVideoBitRate_withMp4() {
         int bitrate = assertDoesNotThrow(() ->
-                Utils.calcVideoBitRate(DebugPaths.videoTestInputMp4));
+                MediaUtils.calcVideoBitRate(DebugPaths.videoTestInputMp4));
         assertTrue(bitrate > 0);
     }
 
     @Test
     void calcVideoBitRate_withMov() {
         int bitrate = assertDoesNotThrow(() ->
-                Utils.calcVideoBitRate(DebugPaths.videoTestInputMov));
+                MediaUtils.calcVideoBitRate(DebugPaths.videoTestInputMov));
         assertTrue(bitrate > 0);
     }
 
     @Test
     void calcVideoBitRate_withAudio() {
         int bitrate = assertDoesNotThrow(() ->
-                Utils.calcVideoBitRate(DebugPaths.audioTestInputMp3));
+                MediaUtils.calcVideoBitRate(DebugPaths.audioTestInputMp3));
         assertTrue(bitrate > 0);
     }
 
     @Test
     void calcVideoBitRate_nonExistent() {
         assertThrows(NoSuchFileException.class,
-                () -> Utils.calcVideoBitRate(tempDir.resolve("none.test")));
+                () -> MediaUtils.calcVideoBitRate(tempDir.resolve("none.test")));
     }
 
     @Test
     void getVideoDimensions_withWebm() {
         VideoSize size = assertDoesNotThrow(() ->
-                Utils.getVideoDimensions(DebugPaths.videoTestInputWebm));
+                VideoUtils.getVideoDimensions(DebugPaths.videoTestInputWebm));
         assertTrue(size.getHeight() > 0);
         assertTrue(size.getWidth() > 0);
     }
@@ -83,7 +90,7 @@ public class VideoUtilsTest {
     @Test
     void getVideoDimensions_withMp4() {
         VideoSize size = assertDoesNotThrow(() ->
-                Utils.getVideoDimensions(DebugPaths.videoTestInputMp4));
+                VideoUtils.getVideoDimensions(DebugPaths.videoTestInputMp4));
         assertTrue(size.getHeight() > 0);
         assertTrue(size.getWidth() > 0);
     }
@@ -91,7 +98,7 @@ public class VideoUtilsTest {
     @Test
     void getVideoDimensions_withMov() {
         VideoSize size = assertDoesNotThrow(() ->
-                Utils.getVideoDimensions(DebugPaths.videoTestInputMov));
+                VideoUtils.getVideoDimensions(DebugPaths.videoTestInputMov));
         assertTrue(size.getHeight() > 0);
         assertTrue(size.getWidth() > 0);
     }
@@ -99,7 +106,7 @@ public class VideoUtilsTest {
     @Test
     void getVideoDimensions_withMkv() {
         VideoSize size = assertDoesNotThrow(() ->
-                Utils.getVideoDimensions(DebugPaths.videoTestInputMkv));
+                VideoUtils.getVideoDimensions(DebugPaths.videoTestInputMkv));
         assertTrue(size.getHeight() > 0);
         assertTrue(size.getWidth() > 0);
     }
@@ -107,52 +114,52 @@ public class VideoUtilsTest {
     @Test
     void getVideoDimensions_withAudio() {
         assertThrows(NullPointerException.class, () ->
-                Utils.getVideoDimensions(DebugPaths.audioTestInputMp3));
+                VideoUtils.getVideoDimensions(DebugPaths.audioTestInputMp3));
     }
 
     @Test
     void getVideoDimensions_nonExistent() {
         assertThrows(EncoderException.class, () ->
-                Utils.getVideoDimensions(tempDir.resolve("none.test")));
+                VideoUtils.getVideoDimensions(tempDir.resolve("none.test")));
     }
 
     @Test
     void getVideoFrameRate_withWebm() {
         int frames = assertDoesNotThrow(() ->
-                Utils.getVideoFrameRate(DebugPaths.videoTestInputWebm));
+                VideoUtils.getVideoFrameRate(DebugPaths.videoTestInputWebm));
         assertTrue(frames > 0);
     };
 
     @Test
     void getVideoFrameRate_withMp4() {
         int frames = assertDoesNotThrow(() ->
-                Utils.getVideoFrameRate(DebugPaths.videoTestInputMp4));
+                VideoUtils.getVideoFrameRate(DebugPaths.videoTestInputMp4));
         assertTrue(frames > 0);
     };
 
     @Test
     void getVideoFrameRate_withMov() {
         int frames = assertDoesNotThrow(() ->
-                Utils.getVideoFrameRate(DebugPaths.videoTestInputMov));
+                VideoUtils.getVideoFrameRate(DebugPaths.videoTestInputMov));
         assertTrue(frames > 0);
     };
 
     @Test
     void getVideoFrameRate_withMkv() {
         int frames = assertDoesNotThrow(() ->
-                Utils.getVideoFrameRate(DebugPaths.videoTestInputMkv));
+                VideoUtils.getVideoFrameRate(DebugPaths.videoTestInputMkv));
         assertTrue(frames > 0);
     };
 
     @Test
     void getVideoFrameRate_withAudio() {
         assertThrows(NullPointerException.class, () ->
-                Utils.getVideoFrameRate(DebugPaths.audioTestInputMp3));
+                VideoUtils.getVideoFrameRate(DebugPaths.audioTestInputMp3));
     };
 
     @Test
     void getVideoFrameRate_nonExistent() {
         assertThrows(EncoderException.class, () ->
-                Utils.getVideoFrameRate(tempDir.resolve("none.test")));
+                VideoUtils.getVideoFrameRate(tempDir.resolve("none.test")));
     }
 }
