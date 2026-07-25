be7adb1184f88cc6a800d6fbe0abf0e9d569ccd5
N0153128 <noisebro@hotmail.com>
2026-07-25 14:18:26 +0100

    MediaPlugin: replaced generic DisarmException calls with IllegalStateException

--- src/main/java/dev/n0153/app/MediaPlugin.java
+++ src/main/java/dev/n0153/app/MediaPlugin.java
@@ -1,19 +1,17 @@
 package dev.n0153.app;
 
-import dev.n0153.app.exceptions.DisarmException;
-
 public interface MediaPlugin {
     GlobalConfig getGlobalConfig();
     public MediaProcessor<?> getProcessor(MediaConfig config);
     public MediaValidator getValidator();
     public MediaConfig getConfig();
-    void register(PluginRegistry registry, MediaConfig config) throws DisarmException;
+    void register(PluginRegistry registry, MediaConfig config) throws IllegalStateException;
     void registerGlobalConfig(GlobalConfig globalConfig);
     String echo();
 
     default boolean ensureGlobalConfig() {
         if (getGlobalConfig() == null) {
-            throw new DisarmException(echo() + " plugin requires Global Config instance to function");
+            throw new IllegalStateException(echo() + " plugin requires Global Config instance to function");
         } else {
             return true;
         }
