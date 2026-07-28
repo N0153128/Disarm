044c75fbd5b61df38fb9c07bd67ad2e3c3a69289
N0153128 <noisebro@hotmail.com>
2026-07-28 13:25:42 +0100

    TextCLI: added param descriptions to all params

--- src/main/java/dev/n0153/app/plugins/video/VideoCLI.java
+++ src/main/java/dev/n0153/app/plugins/video/VideoCLI.java
@@ -21,22 +21,34 @@ public class VideoCLI implements Runnable {
     @CommandLine.ParentCommand
     DisarmCLI inputPath;
 
-    @CommandLine.Option(names = {"-mvd", "--max-video-duration"}, description = "Change max video duration")
+    @CommandLine.Option(names = {"-mvd", "--max-video-duration"}, description =
+            "Changes maximum video duration limit in milliseconds. " +
+                    "Default limit is set to 5 minutes (300,000). Example: -mvd 600000")
     private int maxVideoDuration;
 
-    @CommandLine.Option(names = {"-tvb", "--target-video-bitrate"}, description = "Change target video bitrate")
+    @CommandLine.Option(names = {"-tvb", "--target-video-bitrate"}, description =
+            "Changes output video bitrate, accepts an integer. " +
+                    "Defaults to 8,000,000. Example: -tvb 10000000")
     private int targetVideoBitrate;
 
-    @CommandLine.Option(names = {"-tfr", "--target-frame-rate"}, description = "Change target frame rate")
+    @CommandLine.Option(names = {"-tfr", "--target-frame-rate"}, description =
+            "Changes output frame rate, accepts an integer. " +
+                    "Defaults to 30. Example: -tfr 60")
     private int targetFrameRate;
 
-    @CommandLine.Option(names = {"-tvc", "--target-video-codec"}, description = "Change target video codec")
+    @CommandLine.Option(names = {"-tvc", "--target-video-codec"}, description =
+            "Changes output video codec, accepts a string. " +
+                    "Defaults to an input video file's codec. Example: -tvc h.264")
     private String targetVideoCodec;
 
-    @CommandLine.Option(names = {"-tacv", "--target-audio-codec-video"}, description = "Change target audio codec for video")
+    @CommandLine.Option(names = {"-tacv", "--target-audio-codec-video"}, description =
+            "Changes output audio codec for video, accepts a string. " +
+                    "Defaults to an input video file's audio codec. Example: -tacv ac3")
     private String targetAudioCodec;
 
-    @CommandLine.Option(names = {"-sav1", "--swap-av1"}, description = "Swap AV1 codec with a more performant. Accepted values: vp8 (default), vp9, av1")
+    @CommandLine.Option(names = {"-sav1", "--swap-av1"}, description =
+            "Swaps AV1 codec with a more performant VP8. Accepted values: vp8, vp9, av1. " +
+                    "Defaults to vp8. Example: -sav1 vp9")
     private String swapAV1;
 
     @Override
