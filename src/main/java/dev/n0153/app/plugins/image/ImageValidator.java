3aa24b2e64dee0eb23302d149cf0d11983bd9fba
N0153128 <noisebro@hotmail.com>
2026-08-04 22:51:52 +0100

    ImageValidator: implemented validateLogo() for logo file validation and checkLogoSizeLimit() check

--- src/main/java/dev/n0153/app/plugins/image/ImageValidator.java
+++ src/main/java/dev/n0153/app/plugins/image/ImageValidator.java
@@ -2,6 +2,8 @@ package dev.n0153.app.plugins.image;
 
 import dev.n0153.app.*;
 import dev.n0153.app.exceptions.ValidationException;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ImageValidator implements MediaValidator {
@@ -23,5 +25,20 @@ public class ImageValidator implements MediaValidator {
 
     public boolean checkMeta() {
         return this.config != null && this.context != null;
+    }
+
+    public boolean validateLogo(Path osTargetPath) {
+        if (!checkMeta()) {
+            throw new ValidationException("Plugin meta data is empty");
+        }
+        if (!checkLogoSizeLimit(osTargetPath)) {
+            throw new ValidationException("Logo file size exceeds logo file size limit");
+        }
+        return true;
+    }
+
+    public boolean checkLogoSizeLimit(Path osTargetPath) {
+        long size = Utils.getSize(osTargetPath);
+        return size <= config.getLogoSizeLimit();
     }
 }
